0a697f0bd34af76467cb6ab5621b3e75082fe51e
rakhirakhu <145709056+rakhirakhu@users.noreply.github.com>
2023-11-17 21:02:03 +0530

    Add files via upload

---
+++ CacheError.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bank_Management
+{
+    
+        public class NoCacheAttribute : ActionFilterAttribute
+        {
+            public override void OnResultExecuted(ResultExecutedContext filterContext)
+            {
+                var response = filterContext.HttpContext.Response;
+                response.Cache.SetCacheability(HttpCacheability.NoCache);
+                response.Cache.SetNoStore();
+                base.OnResultExecuted(filterContext);
+            }
+        }
+    
+}
---
+++ Logger.cs
@@ -0,0 +1,31 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bank_Management
+{
+    public class Logger
+    {
+        private static readonly Logger _instance = new Logger();
+        protected ILog monitoringLogger;
+
+        private Logger()
+        {
+            monitoringLogger = LogManager.GetLogger("MonitoringLogger");
+        }
+
+        public static void info(string message)
+        {
+            _instance.monitoringLogger.Info(message);
+
+
+        }
+
+        public static void Error(string message, Exception ex)
+        {
+            _instance.monitoringLogger.Error(message, ex);
+        }
+    }
+}
